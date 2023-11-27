0ccd7a88ed7eb9c65f022d1cb584d92909d54980
이종혁 <whdgur57117@gmail.com>
2023-11-27 17:27:38 +0900

    [Init]Vue project 기본 template

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return { msg: 'Hello' };
+  },
+};
+</script>
+
+<style lang="scss">
+$color: orange;
+h1 {
+  color: $color;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import * as Vue from 'vue';
+import App from './App.vue';
+Vue.createApp(App).mount('#app');
